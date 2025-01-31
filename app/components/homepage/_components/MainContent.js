bb8a2cbe7a521a53e18b8005742cc23ad6720eae
codingshamim <rakibkhanshamim790@gmail.com>
2025-01-31 20:59:21 +0600

    complete the home page

--- app/components/homepage/_components/MainContent.js
+++ app/components/homepage/_components/MainContent.js
@@ -1,3 +1,29 @@
+import { getUser } from "@/actions";
+import { getPosts } from "@/actions/postActions";
+import PostItem from "@/app/(routes)/(profile)/profile/[profileid]/post/_components/PostItem";
+import WhatsMindInput from "./WhatsMindInput";
+
 export default async function MainContent() {
-  return <div className="middle col-span-12 lg:col-span-5"></div>;
+  const feeds = await getPosts();
+  const data = feeds?.data;
+  const user = await getUser();
+  return (
+    <div className="middle col-span-12 lg:col-span-5">
+      <WhatsMindInput avatar={user?.avatar} />
+      {data &&
+        data.map((post) => (
+          <PostItem
+            postUserId={post?.user}
+            postId={post?._id}
+            audience={post?.audience}
+            htmlContent={post?.htmlContent}
+            time={post?.createdAt}
+            postUser={post?.user}
+            user={user?.user}
+            images={post?.postImages}
+            key={post?._id}
+          />
+        ))}
+    </div>
+  );
 }
