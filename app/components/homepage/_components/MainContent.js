4251e0d772042aca32f49ff5a6e825b1b4d2f9be
codingshamim <rakibkhanshamim790@gmail.com>
2025-01-31 20:33:46 +0600

    ....

--- app/components/homepage/_components/MainContent.js
+++ app/components/homepage/_components/MainContent.js
@@ -1,37 +1,3 @@
-import { getUser } from "@/actions";
-import { getPosts } from "@/actions/postActions";
-
 export default async function MainContent() {
-  const feeds = await getPosts();
-  const data = feeds?.data;
-  const user = await getUser();
-
-  return (
-    <div className="middle col-span-12 lg:col-span-5">
-      <Stories />
-
-      {data.length > 0 ? (
-        <>
-          <h2 className="text-xl font-medium">Recent Posts</h2>
-          <div className="pb-4">
-            {data.map((post) => (
-              <PostItem
-                postId={post?._id}
-                postUserId={post?.user?._id}
-                audience={post?.audience}
-                htmlContent={post?.htmlContent}
-                time={post?.createdAt}
-                postUser={post?.user}
-                user={user?.user}
-                images={post?.postImages}
-                key={post?._id}
-              />
-            ))}
-          </div>
-        </>
-      ) : (
-        <h2 className="text-center mt-2">No Posts Found</h2>
-      )}
-    </div>
-  );
+  return <div className="middle col-span-12 lg:col-span-5"></div>;
 }
