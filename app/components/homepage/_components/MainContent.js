04e2f653c067977f880311a52cfb3a73356f9b29
codingshamim <rakibkhanshamim790@gmail.com>
2025-01-31 20:22:43 +0600

    fix the bug

--- app/components/homepage/_components/MainContent.js
+++ app/components/homepage/_components/MainContent.js
@@ -1,19 +1,17 @@
-import PostItem from "@/app/(routes)/(profile)/profile/[profileid]/post/_components/PostItem";
-// import Stories from "./Stories";
 import WhatsMindInput from "./WhatsMindInput";
 import { getUser } from "@/actions";
 import { getPosts } from "@/actions/postActions";
 
 export default async function MainContent() {
   const feeds = await getPosts();
-  const data = feeds?.data;
+  // const data = feeds?.data;
   const user = await getUser();
 
   return (
     <div className="middle col-span-12 lg:col-span-5">
       {/* <Stories /> */}
       <WhatsMindInput avatar={user?.avatar} />
-      {data.length > 0 ? (
+      {/* {data.length > 0 ? (
         <>
           <h2 className="text-xl font-medium">Recent Posts</h2>
           <div className="pb-4">
@@ -34,7 +32,7 @@ export default async function MainContent() {
         </>
       ) : (
         <h2 className="text-center mt-2">No Posts Found</h2>
-      )}
+      )} */}
     </div>
   );
 }
