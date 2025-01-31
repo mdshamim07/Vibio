25ec7ca8f0638b2c87ffccb1523ab5ac5646adc4
codingshamim <rakibkhanshamim790@gmail.com>
2025-01-31 20:27:27 +0600

    fix the more bug

--- app/components/homepage/_components/MainContent.js
+++ app/components/homepage/_components/MainContent.js
@@ -1,17 +1,16 @@
-import WhatsMindInput from "./WhatsMindInput";
 import { getUser } from "@/actions";
 import { getPosts } from "@/actions/postActions";
 
 export default async function MainContent() {
   const feeds = await getPosts();
-  // const data = feeds?.data;
+  const data = feeds?.data;
   const user = await getUser();
 
   return (
     <div className="middle col-span-12 lg:col-span-5">
-      {/* <Stories /> */}
-      <WhatsMindInput avatar={user?.avatar} />
-      {/* {data.length > 0 ? (
+      <Stories />
+
+      {data.length > 0 ? (
         <>
           <h2 className="text-xl font-medium">Recent Posts</h2>
           <div className="pb-4">
@@ -32,7 +31,7 @@ export default async function MainContent() {
         </>
       ) : (
         <h2 className="text-center mt-2">No Posts Found</h2>
-      )} */}
+      )}
     </div>
   );
 }
